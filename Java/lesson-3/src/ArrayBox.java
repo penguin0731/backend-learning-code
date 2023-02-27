bc4299b485a565e6bb66e9ff8a25a2852cef8ba7
penguin0731 <276087919@qq.com>
2023-02-27 16:37:20 +0800

    更新代码

--- Java/lesson-3/src/ArrayBox.java
+++ Java/lesson-3/src/ArrayBox.java
@@ -9,20 +9,21 @@
  * - 没有长度限制
  * - 遍历
  */
-public class ArrayBox {
-    public int[] array = new int[10]; // 数组
-    public int size = 0; // 数组实际长度
+public class ArrayBox<E> { // 泛型
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object[] array; // 数组
+    private int size = 0; // 数组实际长度
 
     private void rangeCheck(int index) {
-        if (index < 0 || index + 1 > this.size) {
+        if (index < 0 || index + 1 > size) {
             // index范围超过了数组实际长度
-            throw new BoxIndexOutOfBoundsException("index:" + index + ",size:" + this.size);
+            throw new BoxIndexOutOfBoundsException("index:" + index + ",size:" + size);
         }
     }
 
     // 将旧数组的元素挪到一个新数组中
-    private int[] copyOf(int[] oldArray, int newCapacity) {
-        int[] newArray = new int[newCapacity];
+    private Object[] copyOf(Object[] oldArray, int newCapacity) {
+        Object[] newArray = new Object[newCapacity];
         for (int i = 0; i < oldArray.length; i++) {
             newArray[i] = oldArray[i];
         }
@@ -32,39 +33,58 @@ public class ArrayBox {
     // 扩容数组
     private void grow(int minCapacity) {
         // 在当前数组长度的基础上，扩容1.5倍
-        int oldCapacity = this.array.length;
+        int oldCapacity = array.length;
         int newCapacity = oldCapacity + (oldCapacity >> 1);
         if (newCapacity < minCapacity) {
             // 如果扩容后的数组长度还比数组实际需要的长度小，则直接使用minCapacity
             newCapacity = minCapacity;
         }
         // 将旧数组的内容挪到新数组中
-        this.array = this.copyOf(this.array, newCapacity);
+        array = this.copyOf(array, newCapacity);
     }
 
     // 判断数组实际需要的长度是否比数组初始化的长度要大
     private void ensureCapacityInternal(int minCapacity) {
         // 如果数组实际需要的长度是否比数组初始化的长度要大，则将数组扩容
-        if (minCapacity > this.array.length) {
+        if (minCapacity > array.length) {
             this.grow(minCapacity);
         }
     }
 
+    public int size() {
+        return size;
+    }
+
     // 添加元素
-    public boolean add(int item) {
+    public boolean add(E item) {
         // 确保当前数组长度能够添加新元素
-        this.ensureCapacityInternal(this.size + 1);
-        this.array[this.size++] = item;
+        this.ensureCapacityInternal(size + 1);
+        array[size++] = item;
         return true;
     }
     // 获取元素
-    public int get(int index) {
+    public E get(int index) {
         // 检查index范围是否合法
         this.rangeCheck(index);
-        return this.array[index];
+        return (E)array[index];
     }
     // 删除元素
-    public boolean delete(int index) {
-        return true;
+    public E delete(int index) {
+        // 检查index范围是否合法
+        this.rangeCheck(index);
+        E oldValue = (E)array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[--size] = null;
+        return oldValue;
+    }
+
+    public ArrayBox() {
+        array = new Object[DEFAULT_CAPACITY];
+    }
+
+    public ArrayBox(int capacity) {
+        array = new Object[capacity];
     }
 }
--- Java/lesson-3/src/Test.java
+++ Java/lesson-3/src/Test.java
@@ -1,11 +1,19 @@
 public class Test {
     public static void main(String[] args) {
-        ArrayBox arrayBox = new ArrayBox();
-        for (int i = 0; i < 5; i++) {
+        ArrayBox<Integer> arrayBox = new ArrayBox<Integer>(5);
+        for (int i = 1; i <= 5; i++) {
             arrayBox.add(10 * i);
         }
-        System.out.println(arrayBox.size);
-        System.out.println(arrayBox.array.length);
-        System.out.println(arrayBox.get(5));
+//        System.out.println(arrayBox.size());
+//        System.out.println(arrayBox.array.length);
+//        System.out.println(arrayBox.get(5));
+
+        int deleteItem = arrayBox.delete(0);
+        System.out.println("删除的是" + deleteItem);
+        System.out.println("size是" + arrayBox.size());
+        for(int i = 0; i < arrayBox.size(); i ++) {
+            System.out.println(arrayBox.get(i));
+        }
+
     }
 }
