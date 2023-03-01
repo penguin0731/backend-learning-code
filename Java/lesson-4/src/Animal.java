41a77ac4427cad585c16d57da55f469c522f3103
penguin0731 <276087919@qq.com>
2023-03-01 14:58:25 +0800

    更新代码

--- Java/lesson-4/src/Animal.java
+++ Java/lesson-4/src/Animal.java
@@ -1,6 +1,10 @@
 public class Animal {
     public String name;
 
+    {
+        System.out.println("Animal的程序块");
+    }
+
     public Animal() {
         System.out.println("Animal的无参构造函数");
     }
