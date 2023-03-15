c1a52bb36a086f04dd55eeb91e0cd23b64e11d7d
penguin0731 <276087919@qq.com>
2023-03-15 11:11:40 +0800

    更新代码

--- Java/lesson-7/src/singleton/Singleton.java
+++ Java/lesson-7/src/singleton/Singleton.java
@@ -5,13 +5,16 @@ public class Singleton {
     // 私有化静态化唯一的实例对象
     // 私有化保护属性
     // 静态化只有加载一次属性
-    private static Singleton singleton = new Singleton();
+    private static Singleton singleton;
 
     // 私有化构造函数，避免开发者通过构造函数创建对象
     private Singleton() {}
 
     // 提供一个方法获取唯一实例
     public static Singleton getSingleton() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
         return singleton;
     }
 }
